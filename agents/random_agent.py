208fced31ff1ada386a848f187d07416c217f25e
Koustuv Sinha <koustuvsinha@hotmail.com>
2022-03-30 17:02:41 -0400

    An example of adding a second agent

--- agents/random_agent.py
+++ agents/random_agent.py
@@ -49,3 +49,53 @@ class RandomAgent(Agent):
             dir = np.random.randint(0, 4)
 
         return my_pos, dir
+
+
+## Copying the random agent to display registering multiple agents
+
+
+@register_agent("random_agent_2")
+class RandomAgent2(Agent):
+    """
+    Example of an agent which takes random decisions
+    """
+
+    def __init__(self):
+        super(RandomAgent2, self).__init__()
+        self.name = "RandomAgent2"
+        self.autoplay = True
+
+    def step(self, chess_board, my_pos, adv_pos, max_step):
+        # Moves (Up, Right, Down, Left)
+        ori_pos = deepcopy(my_pos)
+        moves = ((-1, 0), (0, 1), (1, 0), (0, -1))
+        steps = np.random.randint(0, max_step + 1)
+
+        # Random Walk
+        for _ in range(steps):
+            r, c = my_pos
+            dir = np.random.randint(0, 4)
+            m_r, m_c = moves[dir]
+            my_pos = (r + m_r, c + m_c)
+
+            # Special Case enclosed by Adversary
+            k = 0
+            while chess_board[r, c, dir] or my_pos == adv_pos:
+                k += 1
+                if k > 300:
+                    break
+                dir = np.random.randint(0, 4)
+                m_r, m_c = moves[dir]
+                my_pos = (r + m_r, c + m_c)
+
+            if k > 300:
+                my_pos = ori_pos
+                break
+
+        # Put Barrier
+        dir = np.random.randint(0, 4)
+        r, c = my_pos
+        while chess_board[r, c, dir]:
+            dir = np.random.randint(0, 4)
+
+        return my_pos, dir
